5e9e15e7191d4dc2d25c19c70f5e07d052ef7dbd
João Cardoso <joao.a.cardoso@gmail.com>
2026-06-10 21:36:25 +0100

    Revised ownership and bumpet to v1.3.1

--- __init__.py
+++ __init__.py
@@ -8,8 +8,8 @@ class SendToCalibreWebPlugin(InterfaceActionBase):
     name                   = 'Send to Calibre-web'
     description            = 'Send selected books to a Calibre-web server'
     supported_platforms    = ['windows', 'osx', 'linux']
-    author                 = 'Claude and João Cardoso'
-    version                = (1, 3, 0)
+    author                 = 'João Cardoso and Claude'
+    version                = (1, 3, 1)
     minimum_calibre_version = (6, 0, 0)
 
     actual_plugin = 'calibre_plugins.send_to_calibre_web.action:SendToCalibreWebAction'
